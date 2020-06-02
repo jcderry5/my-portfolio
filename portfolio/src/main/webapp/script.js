f7baefe8dfb129ecadacf04ab401eeb7ed335cfe
Jalyn Derry <jcderry@me.com>
2020-06-02 22:20:34 +0000

    Remove unnecessary comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,7 +16,6 @@
  * Fetches stats from the servers and adds them to the DOM.
  */
 function generateDiscussion() {
-    console.log("Testing Testing Uno Dos Tres");
   	
     fetch('/data').then(response => response.json()).then((discussion) => {
         // discussion is an object, not a string, so we have to
@@ -33,35 +32,6 @@ function generateDiscussion() {
         discussionListElement.appendChild(
         	createListElement('Comment #2: ' + discussion.Comment2));
   	});
-
-    console.log("cuatro cuatro");
-
-    //console.log(responsePromise);
-
-    //responsePromise.then(handleResponse);
-}
-
-function handleResponse(response){
-    console.log('Six is the magic number');
-
-    const firstComment = response.text();
-
-    console.log(firstComment + 'is the firstComment');
-
-    firstComment.then(printComment);
-}
-
-function printComment(comment){
-    console.log('Seven for heaven');
-
-    const discussionListElement = document.getElementById('discussin-container');
-	discussionListElement.innerHTML = '';
-    discussionListElement.appendChild(
-        createListElement('first comment: ' + comments.getSampleComment(0)));
-    
-    console.log('number eight mate');
-
-    console.log(discussionListElement);
 }
 
 /** Creates an <li> element containing text. */
