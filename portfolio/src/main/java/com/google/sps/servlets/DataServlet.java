b282fe3f83c3c947f3e9f17eb2a56d885ecb7ec9
Jalyn Derry <jcderry@me.com>
2020-06-02 18:23:18 +0000

    Link ServerStats to DataServlet to create json of comments

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,19 +14,50 @@
 
 package com.google.sps.servlets;
 
+import com.google.sps.data.ServerStats;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
-public class DataServlet extends HttpServlet {
+public final class DataServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Jalyn!</h1>");
-  }
+	@Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            
+        // Generate the comments for the discussion board in array list
+
+        ArrayList<String> sampleComments = new ArrayList<>();
+        sampleComments.add("You rock! Keep on coding!");
+        sampleComments.add("This is momma! You got this girl :-)");
+        sampleComments.add("This is dad! Go break some eggs, babygirl.");
+
+        // Create serverData object in json form
+        ServerStats serverData = new ServerStats(sampleComments);
+        String json = convertToJson(serverData);
+
+        // Send the JSON as the response
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+
+    }
+
+    /**
+    * Converts a serverData instance into a JSON string using manual String concatentation.
+    */
+    private String convertToJson(ServerStats serverData) {
+        String json = "{";
+
+        // loop through all comments in serverData array and concatenate it to json
+        for(int index = 0; index < serverData.getSampleCommentsSize(); index++){
+            json += "Comment #" + index + ": ";
+            json += serverData.getSampleComment(index) + " ";
+        }
+        json += "}";
+        return json;
+    }
 }
