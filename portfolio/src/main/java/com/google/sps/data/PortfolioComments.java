09a9f877681db3d178d1665fb845972a965aa7e0
Jalyn Derry <jcderry@me.com>
2020-06-05 22:45:45 +0000

    Update documentation and spacing

--- portfolio/src/main/java/com/google/sps/data/PortfolioComments.java
+++ portfolio/src/main/java/com/google/sps/data/PortfolioComments.java
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     https://www.apache.org/licenses/LICENSE-2.0
+//   https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,27 +17,33 @@ package com.google.sps.data;
 import java.util.ArrayList;
 
 
-/** Class containing server statistics. */
+/** Class containing list of Comments for Jalyn's Portfolio */
 public final class PortfolioComments {
 
-	private final ArrayList<String> masterCommentList = new ArrayList<>();
+  private final ArrayList<String> masterCommentList = new ArrayList<>();
 
-	public PortfolioComments() {
-        addComment("This is the first comment in masterCommentList");
-  	}
+  // Creates an instance of PortfolioComments with a hard-coded comment
+  // as the first comment in the list
+  public PortfolioComments() {
+    addComment("This is the first comment in masterCommentList");
+  }
+  
+  // Adds provided comment to the masterCommentList data structure
+  public void addComment(String comment){
+    masterCommentList.add(comment);
+  }
 
-    public void addComment(String comment){
-        masterCommentList.add(comment);
+  // Calling this method will give back the comment at that provided index
+  // OR an IndexOutOfBoundsException if < 0 || > masterCommentList.size()
+  public String getComment(int index){
+    if(index >= masterCommentList.size() || index < 0){
+      throws new IndexOutOfBoundsException("There is not a comment at the index");
     }
+    return masterCommentList.get(index);
+  }
 
-	public String getComment(int index){
-	    if(index >= masterCommentList.size() || index < 0){
-	        throw new IndexOutOfBoundsException("There is not a comment at the index");
-        }
-        return masterCommentList.get(index);
-    }
-
-    public int getMasterCommentListSize(){
-        return masterCommentList.size();
+  // Returns masterCommentList's size
+  public int getMasterCommentListSize(){
+    return masterCommentList.size();
     }
 }
--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     https://www.apache.org/licenses/LICENSE-2.0
+//   https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,44 +22,45 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** Servlet that returns some example content. TODO: modify this file 
+to handle user input instead of hard-coded comments*/
 @WebServlet("/data")
 public final class DataServlet extends HttpServlet {
 
-	@Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        
-        PortfolioComments commentsRecord = new PortfolioComments();
+  /** Takes in hard-coded messages and store them in json **/
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  
+    PortfolioComments commentsRecord = new PortfolioComments();
+    StringBuilder json = new StringBuilder();
 
-        // Add Sample Comments
-        commentsRecord.addComment("You rock! Keep on coding!");
-        commentsRecord.addComment("This is momma! You got this girl :-)");
-        commentsRecord.addComment("This is dad! Go break some eggs, babygirl.");
+    // Add Sample Comments
+    commentsRecord.addComment("You rock! Keep on coding!");
+    commentsRecord.addComment("This is momma! You got this girl :-)");
+    commentsRecord.addComment("This is dad! Go break some eggs, babygirl.");
 
-	    // Create commentsRecord object in json form
-        String json = convertToJson(commentsRecord);
+	// Create commentsRecord object in json form
+    convertToJson(commentsRecord);
 
-        // Send the JSON as the response
-        response.setContentType("application/json;");
-        response.getWriter().println(json);
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json.toString());
+  }
 
+  /**
+  * Converts the commentsRecord instance into a JSON string using manual String concatentation.
+  */
+  private void convertToJson(PortfolioComments commentsRecord) {
+    json.append("{")
+    json.append("\"Comment0\": ");
+  	json.append("\"" + commentsRecord.getComment(0) + "\"");
+
+    // loop through all comments in commentsRecord array and concatenate it to json
+    for(int index = 1; index < commentsRecord.getMasterCommentListSize(); index++){
+      json.append(", ");
+      json.append("\"Comment" + index + "\": ");
+      json.append("\"" + commentsRecord.getComment(index) + "\"");
     }
-
-    /**
-    * Converts the commentsRecord instance into a JSON string using manual String concatentation.
-    */
-    private String convertToJson(PortfolioComments commentsRecord) {
-        String json = "{";
-        json += "\"Comment0\": ";
-    	json += "\"" + commentsRecord.getComment(0) + "\"";
-
-        // loop through all comments in commentsRecord array and concatenate it to json
-        for(int index = 1; index < commentsRecord.getMasterCommentListSize(); index++){
-            json += ", ";
-            json += "\"Comment" + index + "\": ";
-            json += "\"" + commentsRecord.getComment(index) + "\"";
-        }
-        json += "}";
-        return json;
-    }
+    json.append("}");
+  }
 }
